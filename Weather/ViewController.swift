cbc7a65781ac14e83eacda57246122d700a2c02d
Ridwan Abdurrasyid <ridwan.h.abdurrasyid@gmail.com>
2020-01-23 17:27:00 +0700

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ridwan Abdurrasyid on 23/01/20.
+//  Copyright © 2020 ridwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
